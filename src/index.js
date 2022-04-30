b365a5b87fec239225f3f7e1bcc725bf64b50d77
Mark Westerlund <mwesterlund@Workstations-MacBook-Pro.local>
2022-04-29 20:40:57 -0400

    Add base app
    Add simple todo list
    Add simple todo list input
    set up sass
    
    SWENG-431-fp-1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TodoInput from './components/TodoInput/TodoInput';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
